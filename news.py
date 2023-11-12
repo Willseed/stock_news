8538603f0575a289d2febc3517dae924484f1014
willseed <willseed@icloud.com>
2023-11-12 16:41:18 +0800

    Changing got web result to file

--- news.py
+++ news.py
@@ -2,10 +2,23 @@
 import random
 import requests as rq
 import time
+import calendar
+import os
 
 # 導入 BeautifulSoup module: 解析 HTML 語法工具
 from bs4 import BeautifulSoup as BS
 from fake_useragent import UserAgent
+
+# 時間戳轉換成日期
+def timestamp2date(timestamp: str):
+    tmp_list = timestamp.split()
+    month = mapping_month(tmp_list[1])
+    return f'{tmp_list[4]}{month:02d}{int(tmp_list[2]):02d}'
+
+# 英文月份縮寫對應數字
+def mapping_month(month_abbr: str):
+    month_abbr_list = {month: index for index, month in enumerate(calendar.month_abbr) if month}
+    return month_abbr_list[month_abbr]
 
 def find_last_index(content):
     last_index = 0
@@ -14,34 +27,40 @@ def find_last_index(content):
             last_index = index
     return last_index
 
-def extract_content(subURL):
+def extract_content(subURL: str, article: str):
     ua = UserAgent()
     user_agent = ua.random
     headers = {'user-agent': user_agent}
     print(subURL)
+    print(article)
     res = rq.get(subURL, headers=headers)
     soup = BS(res.text, 'html.parser')
     header = soup.select('span.article-meta-value')
     author = header[0].text
     timestamp = header[3].text
-    print(author)
-    print(timestamp)
-    main_container = soup.find(id='main-container')
-    all_text = main_container.text
-    content = all_text.split('\n')
-    last_index = find_last_index(content)
-    final_content = content[0:last_index]
-    print("\n".join(final_content))
-    comment_tag = soup.select('div.push span.push-tag')
-    comment_user = soup.select('div.push span.push-userid')
-    comment_content = soup.select('div.push span.push-content')
-    for i in range(len(comment_tag)):
-        print(f'{comment_tag[i].text.strip()} {comment_user[i].text.strip()}{comment_content[i].text.strip()}')
-
+    date = timestamp2date(timestamp)
+    serial_number = subURL.split('/')[-1]
+    if os.path.isdir('file') == False:
+        os.mkdir('file')
+    with open(f'./file/{date}_{serial_number}.txt', 'w', encoding='utf-8') as f:
+        f.write(subURL + '\n')
+        f.write(article + '\n')
+        f.write(author + '\n')
+        f.write(timestamp + '\n')
+        main_container = soup.find(id='main-container')
+        all_text = main_container.text
+        content = all_text.split('\n')
+        last_index = find_last_index(content)
+        final_content = content[0:last_index]
+        f.write("\n".join(final_content))
+        comment_tag = soup.select('div.push span.push-tag')
+        comment_user = soup.select('div.push span.push-userid')
+        comment_content = soup.select('div.push span.push-content')
+        for i in range(len(comment_tag)):
+            f.write(f'{comment_tag[i].text.strip()} {comment_user[i].text.strip()}{comment_content[i].text.strip()}\n')
 
 # 將 PTT Stock 存到 URL 變數中
 URL = 'https://www.ptt.cc/bbs/Stock/index43.html' 
-
 
 # 使用 for 迴圈將逐筆將標籤(tags)裡的 List 印出, 這裡取3頁
 for round in range(5775):
@@ -70,9 +89,9 @@ for round in range(5775):
         if '[公告]' in article:
             continue
         subURL = 'https://www.ptt.cc' + x['href']
-        print(article)
         try:
-            extract_content(subURL)
+            extract_content(subURL, article)
         except Exception as e:
             print(f'發生異常，異常原因：{e}')
         time.sleep(random.uniform(0.5, 2.5))
+    break
